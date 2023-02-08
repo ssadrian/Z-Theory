1485c14af7cfb20d1ccb9b92d60fac8bad2500b9
ssadrian <113125604+ssadrian@users.noreply.github.com>
2023-02-08 19:15:52 +0100

    Restructure components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LogoComponent } from './logo/logo.component';
-import { FooterComponent } from './footer/footer.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { StudentRegisterFormComponent } from './pages/student-register-form/student-register-form.component';
 import { TeacherRegisterFormComponent } from './pages/teacher-register-form/teacher-register-form.component';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './modules/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
