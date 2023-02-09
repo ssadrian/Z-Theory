6fe68f1120bd506cb5652383c5eb4f69fa979e75
Siru <msrijanamp@gmail.com>
2023-02-09 19:15:15 +0100

    Add student form

--- Frontend/src/app/pages/student-register-form/student-register-form.component.ts
+++ Frontend/src/app/pages/student-register-form/student-register-form.component.ts
@@ -1,14 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import {RegistrationService} from "../../services/registration.service";
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { IFormStudent } from 'src/models/form/form-student';
+import { RegistrationService } from "../../services/registration.service";
 
 @Component({
   selector: 'app-student-register-form',
   templateUrl: './student-register-form.component.html',
   styleUrls: ['./student-register-form.component.scss']
 })
-export class StudentRegisterFormComponent implements OnInit {
-  constructor(private register: RegistrationService) { }
+export class StudentRegisterFormComponent {
+  constructor(private register: RegistrationService, private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  isSubmit: boolean = false
+
+  studentForm = this.fb.group({
+    nickname: ["", [Validators.required]],
+    birth_date: [new Date, [Validators.required]],
+    name: ["", [Validators.required]],
+    surnames: ["", [Validators.required]],
+    password: ["", [Validators.required]],
+    password_confirmation: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.email]],
+    tos: [false, [Validators.requiredTrue]]
+  })
+
+  get formControl(): { [key: string]: AbstractControl } {
+    return this.studentForm.controls;
   }
+
+  submit(): void {
+    this.isSubmit = true;
+
+    if (!this.studentForm.valid) {
+      return;
+    }
+
+    let formValue = this.studentForm.value;
+    let student: IFormStudent = {
+      name: formValue.name!,
+      surnames: formValue.surnames!,
+      nickname: formValue.nickname!,
+      email: formValue.email!,
+      password: formValue.password!,
+      password_confirmation: formValue.password_confirmation!,
+      birth_date: formValue.birth_date!,
+    };
+
+    console.log(JSON.stringify(formValue));
+    console.log(this.register.registerStudent(student));
+  }
+
+
+
 }
