523a2af700808db7114be107ff4b1291b89b2c4e
ssadrian <113125604+ssadrian@users.noreply.github.com>
2023-02-12 20:09:27 +0100

    Add navbar reactivity to anon/logged user

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import {LoginService} from '../../services/login.service';
+import {Component} from "@angular/core";
+import {LoginService} from "../../services/login.service";
+import {CredentialService} from "../../services/credential.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
-export class NavbarComponent implements OnInit {
-  constructor(private loginService: LoginService) { }
+export class NavbarComponent {
+  constructor(private loginService: LoginService, private credentials: CredentialService) {
+  }
 
-  ngOnInit(): void {
+  get isLogged(): boolean {
+    return this.credentials.currentUser !== undefined;
   }
 
   logOut(): void {
