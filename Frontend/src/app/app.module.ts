73d53cc6f288b29f199a8a5a361e34c64dc938b4
Axel <axel.arroyo.gimenez@gmail.com>
2023-03-02 19:50:28 +0100

    Refactor ranking style

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,26 +1,37 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {LogoComponent} from './components/logo/logo.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {MaterialModule} from './modules/material.module';
-import {LandPageComponent} from './pages/land-page/land-page.component';
-import {LoginFormComponent} from './pages/login-form/login-form.component';
-import {NotFoundComponent} from './pages/not-found/not-found.component';
-import {Base64Service} from './services/base64.service';
-import {LoginService} from './services/login.service';
-import {RankingService} from './services/repository/ranking.service';
-import {RegistrationService} from './services/registration.service';
-import {RegistrationFormComponent} from './pages/registration-form/registration-form.component';
-import {StudentProfileComponent} from './pages/student/student-profile/student-profile.component';
-import {TeacherProfileComponent} from './pages/teacher/teacher-profile/teacher-profile.component';
-import {StudentService} from './services/repository/student.service';
-import {TeacherService} from './services/repository/teacher.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from './modules/material.module';
+import { LandPageComponent } from './pages/land-page/land-page.component';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { Base64Service } from './services/base64.service';
+import { LoginService } from './services/login.service';
+import { RankingService } from './services/repository/ranking.service';
+import { RegistrationService } from './services/registration.service';
+import { RegistrationFormComponent } from './pages/registration-form/registration-form.component';
+import { StudentProfileComponent } from './pages/student/student-profile/student-profile.component';
+import { TeacherProfileComponent } from './pages/teacher/teacher-profile/teacher-profile.component';
+import { StudentService } from './services/repository/student.service';
+import { TeacherService } from './services/repository/teacher.service';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -41,10 +52,27 @@ import {TeacherService} from './services/repository/teacher.service';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    TableModule,
+    CalendarModule,
+		SliderModule,
+		DialogModule,
+		MultiSelectModule,
+		ContextMenuModule,
+		DropdownModule,
+		ButtonModule,
+		ToastModule,
+    InputTextModule,
+    ProgressBarModule,
   ],
-  providers: [LoginService, RegistrationService, Base64Service, StudentService, TeacherService, RankingService],
+  providers: [
+    LoginService,
+    RegistrationService,
+    Base64Service,
+    StudentService,
+    TeacherService,
+    RankingService,
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- Frontend/src/app/pages/student/student-profile/student-profile.component.ts
+++ Frontend/src/app/pages/student/student-profile/student-profile.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {IRanking} from '../../../../models/ranking.model';
-import {IStudent} from '../../../../models/student.model';
-import {CredentialService} from '../../../services/credential.service';
-import {RankingService} from '../../../services/repository/ranking.service';
+import { Component, OnInit } from '@angular/core';
+import { Table } from 'primeng/table';
+import { IRanking } from '../../../../models/ranking.model';
+import { IStudent } from '../../../../models/student.model';
+import { CredentialService } from '../../../services/credential.service';
+import { RankingService } from '../../../services/repository/ranking.service';
 
 @Component({
   selector: 'app-student-profile',
@@ -12,16 +13,55 @@ import {RankingService} from '../../../services/repository/ranking.service';
 export class StudentProfileComponent implements OnInit {
   constructor(
     private credentials: CredentialService,
-    private rankingService: RankingService) {
-  }
+    private rankingService: RankingService
+  ) {}
 
+  customers = [
+    { name: 'Amy Elsner', image: 'amyelsner.png' },
+    { name: 'Anna Fali', image: 'annafali.png' },
+    { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+    { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+    { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+    { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+    { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+    { name: 'Onyama Limba', image: 'onyamalimba.png' },
+    { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+    { name: 'Xuxue Feng', image: 'xuxuefeng.png' },
+  ];
+
+  representatives = [
+    { label: 'Unqualified', value: 'unqualified' },
+    { label: 'Qualified', value: 'qualified' },
+    { label: 'New', value: 'new' },
+    { label: 'Negotiation', value: 'negotiation' },
+    { label: 'Renewal', value: 'renewal' },
+    { label: 'Proposal', value: 'proposal' },
+  ];
+
+  statuses = [
+    { label: 'Unqualified', value: 'unqualified' },
+    { label: 'Qualified', value: 'qualified' },
+    { label: 'New', value: 'new' },
+    { label: 'Negotiation', value: 'negotiation' },
+    { label: 'Renewal', value: 'renewal' },
+    { label: 'Proposal', value: 'proposal' },
+  ];
+
+  loading: boolean = true;
+
+  activityValues: number[] = [0, 100];
   student: IStudent = this.credentials.currentUser as IStudent;
   rankings: IRanking[] = [];
 
   ngOnInit(): void {
-    this.rankingService.all()
-      .subscribe((response: IRanking[]): void => {
-        this.rankings = response;
-      });
+    this.rankingService.all().subscribe((response: IRanking[]): void => {
+      this.rankings = response;
+    });
+
+  
+  }
+
+  clear(table: Table) {
+    table.clear();
   }
 }
