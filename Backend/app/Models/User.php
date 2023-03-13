f0083b62175454dab67141261a584b220f2ce7d4
ssadrian <113125604+ssadrian@users.noreply.github.com>
2023-03-13 20:03:19 +0100

    Fix rebase errors

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
