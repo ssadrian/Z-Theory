fb59a3b1bdf57353574e9f56cff24c669344cf38
ssadrian <113125604+ssadrian@users.noreply.github.com>
2023-03-22 20:19:52 +0100

    Add some UI changes

--- Frontend/src/app/modules/primeng.module.ts
+++ Frontend/src/app/modules/primeng.module.ts
@@ -7,6 +7,7 @@ import {CardModule} from 'primeng/card';
 import {ContextMenuModule} from 'primeng/contextmenu';
 import {DialogModule} from 'primeng/dialog';
 import {DropdownModule} from 'primeng/dropdown';
+import {FileUploadModule} from 'primeng/fileupload';
 import {InputTextModule} from 'primeng/inputtext';
 import {MultiSelectModule} from 'primeng/multiselect';
 import {PasswordModule} from 'primeng/password';
@@ -15,6 +16,8 @@ import {RippleModule} from 'primeng/ripple';
 import {SliderModule} from 'primeng/slider';
 import {TableModule} from 'primeng/table';
 import {ToastModule} from 'primeng/toast';
+import {ToolbarModule} from 'primeng/toolbar';
+import {StyleClassModule} from 'primeng/styleclass';
 
 @NgModule({
   declarations: [],
@@ -34,6 +37,9 @@ import {ToastModule} from 'primeng/toast';
     CardModule,
     PasswordModule,
     RippleModule,
+    ToolbarModule,
+    FileUploadModule,
+    StyleClassModule
   ],
   exports: [
     TableModule,
@@ -50,7 +56,10 @@ import {ToastModule} from 'primeng/toast';
     CardModule,
     PasswordModule,
     RippleModule,
-  ]
+    FileUploadModule,
+    ToolbarModule,
+    StyleClassModule
+  ],
 })
 export class PrimengModule {
 }
