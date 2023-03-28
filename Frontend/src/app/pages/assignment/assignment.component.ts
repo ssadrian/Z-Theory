2f8a673be1c60d3c2dd6878bc29c4d481e165d23
Axel <axel.arroyo.gimenez@gmail.com>
2023-03-28 20:16:35 +0200

    Add assignment component

---
+++ Frontend/src/app/pages/assignment/assignment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment',
+  templateUrl: './assignment.component.html',
+  styleUrls: ['./assignment.component.scss']
+})
+export class AssignmentComponent {
+
+}
