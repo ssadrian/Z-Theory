d27b9f85a05ae79501c6fd0a9305c97e3dc7a091
ssadrian <113125604+ssadrian@users.noreply.github.com>
2023-04-20 17:59:14 +0200

    Configure scheduled kudos task and timezone

--- Backend/app/Console/Kernel.php
+++ Backend/app/Console/Kernel.php
@@ -16,6 +16,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            \App\Models\Student::query()
+                ->update([ 'kudos' => env('WEEKLY_KUDOS') ]);
+        })->weekly();
     }
 
     /**
